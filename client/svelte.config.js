68306a98f71b9de1b321eaac8d27361d7155e5cf
fpaboim <1982065+fpaboim@users.noreply.github.com>
2021-07-23 21:43:39 -0300

    fix tailwind jit

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -4,16 +4,18 @@ import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: [
-    preprocess({
-      postcss: true
-    }),
-  ],
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
     adapter: vercel(),
     vite: {
+      server: {
+        hmr: {
+          host: 'localhost',
+          port: 15000,
+          protocol: 'ws'
+        }
+      },
       resolve: {
         alias: {
           $fabo: path.resolve('./src/lib/.fabo/'),
@@ -21,10 +23,9 @@ const config = {
       }
     }
   },
+  preprocess: [preprocess({
+      "postcss": true
+  })]
 };
 
 export default config;
-// Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
-const mode = process.env.NODE_ENV;
-const dev = mode === "development";
-process.env.TAILWIND_MODE = dev ? "watch" : "build";
