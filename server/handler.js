6b10a7c4f49f114133f10657a3f9e3822cc5e32a
fpaboim <1982065+fpaboim@users.noreply.github.com>
2021-07-30 15:46:46 -0300

    fixes for working with published monorepo

--- server/handler.js
+++ server/handler.js
@@ -2,6 +2,15 @@ import {makeHandler} from 'fabo/packages/server-core'
 import setupPassport from './services/passport.js'
 import router from './.fabo/router.js'
 
-const startApp = makeHandler(router, [setupPassport])
+const fabo_options = {
+  whitelist: [
+    'https://fabo-starter.vercel.app',
+    'http://fabo-starter.vercel.app',
+    'http://192.168.111.3:3000',
+    'http://localhost:3000',
+  ]
+}
+
+const startApp = makeHandler(router, [setupPassport], fabo_options)
 
 export {startApp}
